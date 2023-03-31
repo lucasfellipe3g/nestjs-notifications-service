75bacb7dd3d0150d44e4787477d83be61da87a24
Lucas Fellipe <lucas.fellipe.c@gmail.com>
2023-03-30 21:47:24 -0300

    feat: Finish notifications service

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,4 +1,4 @@
-import { NotificationsRepository } from '@/application/repositories/notifications-repository';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
